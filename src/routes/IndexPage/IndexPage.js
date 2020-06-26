406eaa55c7a1fef8c4f5c5f078c2243c5d3088d6
wanghuajin <wanghuajing@maka.im>
2020-06-26 20:58:56 +0800

    进销存系统

---
+++ src/routes/IndexPage/IndexPage.js
@@ -0,0 +1,18 @@
+﻿import React, {Component, PropTypes} from 'react';
+import {connect} from 'dva';
+import {Link} from 'dva/router';
+import {normal, title, welcome, list} from './index.css';
+
+function IndexPage() {
+    return (
+        <div className={normal}>
+            <h1 className={title}>源隆行酒业进销库存管理系统</h1>
+            <div className={welcome}/>
+            {}
+        </div>
+    );
+}
+
+IndexPage.propTypes = {};
+
+export default connect()(IndexPage);
